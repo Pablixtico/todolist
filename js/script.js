0a2cb90c9983e7c7130c6a7390543e60db9bbbc2
Pablixtico <pablo.gimenez.ricart@gmail.com>
2024-03-14 22:30:22 +0100

    Multiple list added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,39 @@
 var storage;
-storage = JSON.parse(window.localStorage.getItem("1"));
+var actualList = 1;
 
-console.log(-window.localStorage.getItem("2"));
+function clearList(){
+  $(".list-item").each((index, item) => {
+    $(item).remove();
+  });
+}
 
-
-
-
-
-
+function cargarDatos(){
+  clearList();
+  storage = JSON.parse(window.localStorage.getItem(actualList+5));
 $(storage).each((e, item) => {
   $(".list-items").append(
     `<div class="list-item"><div class="list-item-text"><input type="checkbox" name="li1" id="li1"><div class="text">${item}</div></div><span class="material-symbols-outlined cross">close</span></div>`
   );
 })
 let elements = $(".list-item-text input");
-if(window.localStorage.getItem("2") != 0){
-  elements.slice(-window.localStorage.getItem("2")).each((num, item) =>{
+if(window.localStorage.getItem(actualList) != 0){
+  elements.slice(-window.localStorage.getItem(actualList)).each((num, item) =>{
     $(item).prop("checked", "true");
     $(item).parent().css({ "text-decoration": "line-through", color: "red" });
   })
 }
+}
+
+ cargarDatos();
+
+$(".selector-item").click((e) => {
+  console.log("hola")
+  $(".active-selector").removeClass("active-selector");
+  $(e.target).addClass("active-selector")
+  actualList = parseInt($(e.target).text());
+  console.log(actualList);
+  cargarDatos()
+})
 
 $(".search-bar button").click((e) => {
   let text = $(".search-bar input").val();
@@ -102,6 +116,6 @@ function actualizarLista(){
         x++;
     }
   });
-  window.localStorage.setItem("2", x);
-  window.localStorage.setItem("1", JSON.stringify(items));
+  window.localStorage.setItem(actualList, x);
+  window.localStorage.setItem(actualList+5, JSON.stringify(items));
 }
